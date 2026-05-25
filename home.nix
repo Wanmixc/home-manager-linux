743f6c4b98fe8f77f0d134b66473bd2fd8e77011
wanmixc <wanmixc@gmail.com>
2026-05-25 04:10:39 +0000

    🧰update(sort dir & file)

--- home.nix
+++ home.nix
@@ -167,9 +167,9 @@ in
         ls     = "eza --icons";
         clear  = "printf '\\033[2J\\033[3J\\033[1;1H'";
         q      = "qs -c ii";
-        ll     = "eza --icons -T -L 1";
-        l1     = "eza --icons -T -L 1";
-        l2     = "eza --icons -T -L 2";
+        ll     = "eza --icons --group-directories-first -T -L 1";
+        l1     = "eza --icons --group-directories-first -T -L 1";
+        l2     = "eza --icons --group-directories-first -T -L 2";
       };
 
       functions = {
